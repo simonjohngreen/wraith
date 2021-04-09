72f9934ec37ed3d5269cd077a21e6fd4ef8872fd
simonjohngreen <mr.simon.john.green@gmail.com>
2021-04-09 21:03:45 +0100

    adding more detailed logs to the runners

--- deployers/nmap-runner.sh
+++ deployers/nmap-runner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 #redirect stdout/stderr to a file
-exec &> /var/log/wraith.log
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/var/log/wraith.log 2>&1
 cd /root
 #generate the IPV4 nmap reports
 mkdir -p reports-xml
